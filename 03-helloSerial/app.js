5eaf70e41ae56cac80616d5d59ecc36863c6c4cf
debeli <djolex@gmail.com>
2015-05-19 09:55:20 +0200

    Changed code

--- 03-helloSerial/app.js
+++ 03-helloSerial/app.js
@@ -1,33 +1,31 @@
-var SerialPort = require("serialport").SerialPort;
-var port = "/dev/ttyMFD1";
-var serialPort = new SerialPort(port, {
-  baudrate: 115200
-}, false);
+var SerialPort = require("serialport").SerialPort;  
+var port = "/dev/ttyMFD1";  
+var serialPort = new SerialPort(port, {  
+  baudrate: 9600 
+}, false);  
 
-console.log("Open port: "+ port);
-serialPort.open(function (error) {
-  if (error) {
-    console.log('Failed to open: '+error);
-  } else {
-    console.log('open');
-    serialPort.on('data', function(data) {
-      console.log("data recieved: " + data);
-    });
-    ///*while (true) {
-    //serialPort.write("ejlelele igoreu\r"); }*/
-    // write data to serial port every second
-    /*var counter = 90;
-    setInterval(function () {
-      serialPort.write(String(counter)+ "\r\n", function(err) {
-        if(err) {
-          console.log('err ' + err);
-        }else{
-          console.log('Writing data ');
-        }
-      });
-      counter++;
-      if(counter>100)
-        counter =90;
-    }, 1000);*/
-  }
+console.log("Open port: "+ port);  
+serialPort.open(function (error) {  
+  if (error) {  
+    console.log('Failed to open: '+error);  
+  } else {  
+    console.log('open');  
+    serialPort.on('data', function(data) {  
+      console.log('data received: ' + data);  
+    });  
+    //write data to serial port every second  
+    var counter = 90;  
+    setInterval(function () {  
+      serialPort.write(String(counter)+ "\r\n", function(err) {  
+        if(err) {  
+          console.log('err ' + err);  
+        }else{  
+          console.log('Writing data ');  
+        }  
+      });  
+      counter++;  
+      if(counter>100)  
+        counter =90;  
+    }, 1000);  
+  }  
 });
