fc25676d53f2e22e471933df4570c3c38b36d881
debeli <djolex@gmail.com>
2015-05-19 14:34:47 +0200

    Add 05-helloPubNub

---
+++ 05-helloPubNub/app.js
@@ -0,0 +1,15 @@
+var pubnub = require("pubnub")({
+    ssl           : true,  // <- enable TLS Tunneling over TCP 
+    publish_key   : "pub-c-710eb30d-6027-40bb-bcc5-aca1bf4fc7a0",
+    subscribe_key : "sub-c-ace45594-fe18-11e4-bb05-02ee2ddab7fe"
+});
+
+pubnub.publish({
+	channel : 'novisadhardnsoft' ,
+	message : "PROBA!",
+	callback : function (e) {console.log ( "uspelo :D",e);},
+	error : function (e) {console.log ("Nije uspelo! :(",e);} 
+})
+
+
+
