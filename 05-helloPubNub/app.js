5bdadcda3c97a9e49cb48d990975e32cc32f707d
Bojan Čoka <metropolis.lights@gmail.com>
2015-05-19 15:42:32 +0200

    Fix PubNub publishing.

--- 05-helloPubNub/app.js
+++ 05-helloPubNub/app.js
@@ -1,21 +1,16 @@
 var pubnub = require("pubnub")({
-    ssl           : true,  // <- enable TLS Tunneling over TCP 
+    ssl           : true,  // <- enable TLS Tunneling over TCP
     publish_key   : "pub-c-710eb30d-6027-40bb-bcc5-aca1bf4fc7a0",
     subscribe_key : "sub-c-ace45594-fe18-11e4-bb05-02ee2ddab7fe"
+  });
+
+
+var msg = "Test!";
+
+pubnub.publish
+({
+  channel : 'novisadhardnsoft' ,
+  message : msg,
+  callback : function (e) {console.log ( "uspelo :D",e);},
+  error : function (e) {console.log ("Nije uspelo! :(",e);}
 });
-
-
-var msg = {"Test!"};
-function(error){
-	if(error) {console.log('Fail!\n' + error);}
-	else
-	{
-		pubnub.publish({
-		channel : 'novisadhardnsoft' ,
-		message : msg,
-		callback : function (e) {console.log ( "uspelo :D",e);},
-		error : function (e) {console.log ("Nije uspelo! :(",e);}} 
-})}
-
-
-
