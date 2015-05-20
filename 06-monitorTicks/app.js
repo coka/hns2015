f307048302d2b70d82a6566cfb9a043f28493252
debeli <djolex@gmail.com>
2015-05-20 15:52:23 +0200

    code change

--- 06-monitorTicks/app.js
+++ 06-monitorTicks/app.js
@@ -1,26 +1,23 @@
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : "pub-c-710eb30d-6027-40bb-bcc5-aca1bf4fc7a0",
-    subscribe_key : "sub-c-ace45594-fe18-11e4-bb05-02ee2ddab7fe"
-  });
+    publish_key   : "pub-c-93ff324d-77f1-4abe-8c6b-accfcf68e1c3",
+    subscribe_key : "sub-c-aca0eeb0-fdf2-11e4-b699-02ee2ddab7fe"
+});
 
 // U data upisujemo message koji ce biti poslat na pubnub
-  var msg = "Sve sto smo dobili sa seriske"
+  var msg = "Sve sto smo dobili sa seriske";
 
 pubnub.subscribe ({
   	channel: 'Live',
-  	connect: function(res){
-  		console.log('connected');
-  	},
-  	disconnect: function(res){console.log('disconnected');},
-  	reconnect: function(res){conosle.log('reconnecting to pubnub')},
-
-  	message: function(data){console.log(data)}
-  });
+   	message: function(data){console.log(data);}
+});
 
   pubnub.publis({
   	channel: 'Live',
-  	message: msg
-  });
+  	message: msg,
+  	callback  : function(e) { console.log( "SUCCESS!", e ); },
+    error     : function(e) { console.log( "FAILED! RETRY PUBLISH!", e ); }
+});
+  
 
   
