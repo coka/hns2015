1036ff9c9159de07f5fb4f72829df8678fe86f3a
debeli <djolex@gmail.com>
2015-05-20 16:14:41 +0200

    change

--- 06-monitorTicks/app.js
+++ 06-monitorTicks/app.js
@@ -1,7 +1,7 @@
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : "pub-c-93ff324d-77f1-4abe-8c6b-accfcf68e1c3",
-    subscribe_key : "sub-c-aca0eeb0-fdf2-11e4-b699-02ee2ddab7fe"
+    publish_key   : "demo",
+    subscribe_key : "demo"
 });
 
 // U data upisujemo message koji ce biti poslat na pubnub
