73d6f5e215389b9480a806e7c8859a59c769dc1d
Bojan Čoka <metropolis.lights@gmail.com>
2015-05-21 13:03:41 +0200

    Initial refactoring of Final.js.

--- 00-helloAdina/Final.js
+++ 00-helloAdina/Final.js
@@ -1,51 +1,74 @@
-/////////////////////	Requires
+/*
+ * 00-helloAdina
+ */
 
-//	SERIAL
-var SerialPort = require("serialport").SerialPort;
-var port = "/dev/ttyMFD1";
-var serialPort = new SerialPort(port,{baudrate:115200},false);
+// inits
+var SerialPort = require('serialport').SerialPort;
+var port = '/dev/ttyMFD1'; // breakout board's UART1 device name
+var baud = 115200; // can be changed, as long as it's in sync with Arduino Uno
+var opensImmediately = false;
+var serialPort = new SerialPort(port, { baudrate : baud }, opensImmediately);
 
-//	TWITTER
 var Twitter = require('twitter');
-
-var client = new Twitter({
-  consumer_key: 'o13x18XJMMpljgXFdN08MFIsz',
-  consumer_secret: 'TIljUZnvanbSya99jiJLZ9Cy0WT498kwZ7uyMVd8BvroasUovP',
-  access_token_key: '3288108405-CWzNWQd4Q1hrBwlbHlTKkMbT9KLIiwlh6QMFbMd',
+var twitterClient = new Twitter
+({
+  consumer_key       : 'o13x18XJMMpljgXFdN08MFIsz',
+  consumer_secret    : 'TIljUZnvanbSya99jiJLZ9Cy0WT498kwZ7uyMVd8BvroasUovP',
+  access_token_key   : '3288108405-CWzNWQd4Q1hrBwlbHlTKkMbT9KLIiwlh6QMFbMd',
   access_token_secret: 'vn8ku4ERGMMrG4FmCp7xJKDuofKqomlldgBwWwQfT6kz8'
 });
 
-//	PUBNUB
-var pubnub = require("pubnub")({
-    ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : "pub-c-b38d8e57-ce7b-4896-9795-db9a2c494e1c",
-    subscribe_key : "sub-c-fcb17716-fefc-11e4-bb05-02ee2ddab7fe"
+var PubNub = require('pubnub')
+({
+  ssl           : true,  // enable TLS tunneling over TCP
+  publish_key   : 'pub-c-b38d8e57-ce7b-4896-9795-db9a2c494e1c',
+  subscribe_key : 'sub-c-fcb17716-fefc-11e4-bb05-02ee2ddab7fe'
 });
 
-/////////////////////
+// globals
+var tags = ["TEMP", "KORAK", "INFR", "HUMY", "SOUND", "RFID"];
 
-//Start
+console.log('Opening a port at '+ port + '...'); // checkpoint
 
-console.log("Starting...."+ port);
-
-serialPort.open(function (error){
-	if (error) {
-    console.log('Failed to open: '+error);
-  } else
+var parseSerialData = function(dataString, tagArray)
+{
+  // TODO: var output; (JSON)
+  for (i = 0; i < tags.length; i++)
   {
-    console.log('Open:');
-    serialPort.on('data', function(data) {
-    	console.log('Data recived: ' + data);
+    // TODO: parser ignore logic
+    var tag = tags[i];
+    var split = dataString.split(tag);
+    var value = split[1];
+    dataString = split[2];
+  console.log(tag + ":" + value); // this should be output+=...
+}
+  // return output;
+};
 
-    	var tags = ["TEMP", "KORAK", "INFR", "HUMY","SOUND","RFID"];
+var serialEvent = function(data) // callback argument
+{
+  console.log('Data recived: ' + data);
+  for (i = 0; i < tags.length; i++)
+  {
+   var tag = tags[i];
+   var split = data.split(tag);
+   var value = split[1];
+   data= split[2];
+   console.log(tag + ":" + value);
+ }
+};
 
-		for (i = 0; i < tags.length; i++)
-		{
-  			var tag = tags[i];
-  			var split = data.split(tag);
-  			var value = split[1];
-  			data= split[2];
-  			console.log(tag + ":" + value);
-		} });
+// upon receving data through a serial port
+serialPort.open(function (error)
+{
+  if (error) { console.log('Failed to open port.\n Reason: ' + error); }
+  else
+  {
+    serialPort.on('data', serialEvent);
   }
 });
+
+// tweet every minute
+/*
+ * TODO
+ */
